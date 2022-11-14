d15d855342bb032f0ede5485faf6f5f347f34626
Andrés Pardo Rodríguez <apardor@gmail.com>
2022-11-14 14:57:47 +0100

    first push to netlify

---
+++ pages/api/products.js
@@ -0,0 +1,17 @@
+
+
+
+
+// export async function getStaticProps () {
+//   const res = await fetch('https://.../posts/' + context.params.id)
+//   return {
+//     props: {
+//       post: await res.json(),
+// }, }
+// }
+// function Blogpost({ post }) {
+//   return <h1> {post.title} </h1>
+// }
+// export default Blogpost
+
+
